ce1e14e7bf94b589e10c6e983add978492937b71
Charel Felte <charel.felten@gmail.com>
2021-10-06 19:54:05 +0200

    script done

--- processing/fastreader.py
+++ processing/fastreader.py
@@ -1,62 +1,90 @@
-# outstring = ""
-# max_iterations = 10 * 1000 * 100
+import pandas as pd
 
-# code_to_hour_dic = {letter:str(num) for num,letter in enumerate("ABCDEFGHIJKLMNOPQRSTUVWXYZ")}
+def fastreader(input_file, output_file, maxlines = 10 * 1000 * 1000 * 1000):
+    """ fastest way to convert original wikipedia file into cleaned parquet
+        - processing speed: ~250K lines per second
 
-# with open(bigfilepath) as infile:
-#     for i, line in tqdm(enumerate(infile)):
-#         # split line into list of words
-#         wordlist = line.split(" ")
+        input:
+        - input_file:
+            - uncompressed file from wikipedia
+            - example: "../data/pageviews-20210101-automated"
+            - it is important that the filename is exactly as in the article, so pageviews-[date]-[traffic_type] and without extension (no .txt)
+        - output_file:
+            - must either end in ".parquet" or be a directory
+            - if its a directory, then the inputfilename is used and the file is written to the directory
+            - example: "../data/temp/out.parquet"
+            - example: "../data/temp/" --> directory --> outputfile will be: "../data/temp/[input_file_name].parquet"
+        - maxlines: the max number of rows, safety feature for testing on big files. 
+    """
+    
+    code_to_hour_dic = {letter:str(num) for num,letter in enumerate("ABCDEFGHIJKLMNOPQRSTUVWXYZ")}
 
-#         if len(wordlist) == 6:
-#             # the case where we have an ID
-#             if wordlist[3].isnumeric():
-#                 # the case where ID is at correct position
-#                 continue
-#             else:
-#                 # since there is an ID, we need to check if it is null or whether it is the article title and is switched with id
-#                 # null 1234 --> article is null, with ID 1234 --> UNSOVABLY AMBIGIOUS
-#                 # 1234 null --> article is 1234, with ID null --> UNSOVABLY AMBIGIOUS
-#                 # --> for the above case, we will simply take as is, and hope its correct
-#                 # abcd null --> article is abcd, with ID null
-#                 # null abcd --> article is abcd, with ID null, places were switched
-#                 pass  # for now
-#         else:
-#             # the case where we do not have an ID
-#             wordlist.insert(2, "null")
+    input_file_name = input_file.split("/")[-1]
+    input_file_name_components = input_file_name.split("-")
+    traffic_type = input_file_name_components[2]
+    yyyymmdd = input_file_name_components[1]
 
-#         # now we divide to days
-#         wordlists = []
+    if output_file.endswith("/"):
+        # output file is a directory
+        outpuf_file = output_file + input_file_name + ".parquet"
 
-#         # divide last string into separate entries
-#         count = ""
-#         time = ""
-#         first = True
-#         for symbol in wordlist[-1]:
-#             if symbol.isalpha():
-#                 if not first:
-#                     # if this is not the first iteration, count is a valid number and time has also been set
-#                     # since its not the first, we just encountered a letter again, hence we write to the wordlists
-#                     # we write the original list, without the A1B2C3 string and instead encoding the first pair A1 to time: 0, count: 1
-#                     wordlists.append(wordlist[:-1] + [time, count])
-#                     count = ""
-#                 else: first = False  # if it was the first, the next one wont be the first
-#                 # in any case we get the time                    
-#                 time = code_to_hour_dic[symbol]
-#             elif symbol.isnumeric():
-#                 count += symbol
-#             elif symbol == "\n":
-#                 # we arrived at the end
-#                 wordlists.append(wordlist[:-1] + [time, count])
-#                 break
-#         # now we have a wordlists which contains the counts split by hour
+    if output_file[-8:] != ".parquet":
+        print("ERROR: outputfile is not a valid parquet file!")
 
-#         # recombine cleaned list of words into line
-#         for wordlist in wordlists:
-#             print(wordlist)
-#             outstring += " ".join(wordlist) + "\n"
-#         if i > max_iterations:
-#             break
+    outlist = []
 
-# with open(outputfilepath, "w") as outfile:
-#     outfile.write(outstring)
+    print(f"begin processing on file: {input_file}")
+    with open(input_file) as infile:
+        for i, line in enumerate(infile):
+
+            # printing
+            if i > maxlines:
+                break
+            if ((i % 1000000) == 0):
+                print(f"lines read: {(i//1000000)+1}M")
+
+            # split line into list of words
+            wordlist = line.split(" ")
+
+            if len(wordlist) == 5:
+                # the case where we do not have an ID
+                wordlist.insert(2, "null")
+
+            if wordlist[2] == "null":
+                wordlist[2] = "-1"
+
+            wordlists = []
+            count = ""
+            time = ""
+            first = True
+            for symbol in wordlist[-1]:
+                if symbol.isalpha():
+                    if not first:
+                        # if this is not the first iteration, count is a valid number and time has also been set
+                        # since its not the first, we just encountered a letter again, hence we write to the wordlists
+                        # we write the original list, without the A1B2C3 string and instead encoding the first pair A1 to time: 0, count: 1
+                        wordlists.append(wordlist[:-1] + [time, count])
+                        count = ""
+                    else: first = False  # if it was the first, the next one wont be the first
+                    # in any case we get the time                    
+                    time = code_to_hour_dic[symbol]
+                elif symbol.isnumeric():
+                    count += symbol
+                elif symbol == "\n":
+                    # we arrived at the end
+                    wordlists.append(wordlist[:-1] + [time, count])
+                    break
+            # now we have a wordlists which contains the counts split by hour
+            outlist += wordlists
+
+    print("convert list to dataframe")
+    # now we put outlist into a pandas DataFrame and then save as parquet
+    df = pd.DataFrame(outlist, columns=["domain", "title", "id", "access_type", "total", "hour", "count"])
+
+    df["year"] = yyyymmdd[0:4]
+    df["month"] = yyyymmdd[4:6]
+    df["day"] = yyyymmdd[6:8]
+    df["traffic_type"] = traffic_type
+
+    print(f"write parquet to file: {output_file} (may take some time due to partitioning)")
+    df.to_parquet(output_file, partition_cols=["year", "month", "day"])
