23938fd3c3d87ca05a2a3c89f22d4c42638edc85
charel.felten@gmail.com <charel.felten@gmail.com>
2021-10-08 16:38:19 +0000

    push converter

--- processing/converter.py
+++ processing/converter.py
@@ -9,10 +9,22 @@
 
 # COMMAND ----------
 
+from fastreader import fastreader
+
 inputfilepath = "/dbfs/mnt/lsde/wikimedia/pageview_complete/2018/2018-08/pageviews-20180801-spider.bz2"
-outputfilepath = "/dbfs/mnt/group09/pageview_complete/test/test.parquet"
+outputfilepath = "/dbfs/mnt/group09/pageview_complete/test.parquet"
 
+files = [(inputfilepath, outputfilepath)]
 
+rdd = sc.parallelize(files)
+df = rdd.map(fastreader).toDF().display()
+
+# fastreader(inputfilepath, outputfilepath)
+
+# COMMAND ----------
+
+# MAGIC %sh
+# MAGIC ls ..
 
 # COMMAND ----------
 
