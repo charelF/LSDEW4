242cd4bc1233d734073af16887504d54e3f816a9
gilles.magalhaes@gmail.com <gilles.magalhaes@gmail.com>
2021-10-09 17:22:03 +0000

    Clean up downloading notebook

--- dataset-download.py
+++ dataset-download.py
@@ -1,4 +1,11 @@
 # Databricks notebook source
+# MAGIC %md
+# MAGIC 
+# MAGIC # Crawl 
+# MAGIC This notebooks contains code to crawl the `pageviews_complete` dataset from the Wikimedia servers.
+
+# COMMAND ----------
+
 !pip install requests
 
 # COMMAND ----------
@@ -8,10 +15,12 @@ from pyspark.sql.session import SparkSession
 from pathlib import Path
 import requests
 import time
+import json
+import re
 
 # COMMAND ----------
 
-download_links = open("/dbfs/mnt/group09/download_links.txt").read().split("\n")
+download_links = open("/dbfs/mnt/group09/download_links.txt").read().strip().split("\n")
 
 # COMMAND ----------
 
@@ -39,7 +48,7 @@ def download_file(url):
         print(f"{e} occured, waiting {exp_wait}s")
         time.sleep(exp_wait)
         exp_wait *= 2
-        if exp_wait > 300: # don't wait longer than 10 minutes
+        if exp_wait > 300: # don't wait longer than 5 minutes
           return (0,)
 
   f = open(filename+".OK", "w")
@@ -53,9 +62,19 @@ spark = SparkSession \
   .config("spark.dynamicAllocation.enabled", "true") \
   .getOrCreate()
 
-rdd = spark.sparkContext.parallelize(download_links)#.repartition(100)
+rdd = spark.sparkContext.parallelize(download_links)
 df = rdd.map(download_file).toDF()
 df.groupby(df[0]).sum().display()
+
+# COMMAND ----------
+
+download_file("https://dumps.wikimedia.org/other/pageview_complete/monthly/2018/2018-01/pageviews-201801-spider.bz2")
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC 
+# MAGIC Count `*.OK` files to quantify the download process.
 
 # COMMAND ----------
 
@@ -67,3 +86,69 @@ for year_path in dbutils.fs.ls("/mnt/lsde/wikimedia/pageview_complete/"):
         ok_paths.append(daily_path.path)
 
 len(ok_paths)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC 
+# MAGIC ### Basic dataset validation
+# MAGIC 
+# MAGIC Ensure that the downloaded size corresponds to the expected download size.
+
+# COMMAND ----------
+
+files = json.loads(open("/dbfs/mnt/group09/sizes.json").read())
+sizes = {"/".join(f['file'].split("/")[4:]): f['size'] for f in files if re.search(r"20(1[8-9]|2[0-1])-", f["file"])}
+
+verified_urls = set()
+invalid_urls = set()
+
+# COMMAND ----------
+
+for path, size in sizes.items():
+  if path in verified_urls or path in invalid_urls:
+    continue
+
+  if dbutils.fs.ls("/mnt/lsde/wikimedia/"+path)[0].size == size:
+    verified_urls.add(path)
+  else:
+    invalid_urls.add(path)
+
+len(invalid_urls) == 0
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC 
+# MAGIC ## Clean up OK files
+
+# COMMAND ----------
+
+ok_count = 0
+for year_path in dbutils.fs.ls("/mnt/lsde/wikimedia/pageview_complete/"):
+  for month_path in dbutils.fs.ls(year_path.path):
+    for daily_path in dbutils.fs.ls(month_path.path):
+      if daily_path.path.endswith(".OK"):
+        dbutils.fs.rm(daily_path.path)
+        ok_count += 1
+print(ok_count)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC 
+# MAGIC ## Compute total size of dataset in MiB
+
+# COMMAND ----------
+
+total_size = 0
+for year_path in dbutils.fs.ls("/mnt/lsde/wikimedia/pageview_complete/"):
+  for month_path in dbutils.fs.ls(year_path.path):
+    for daily_path in dbutils.fs.ls(month_path.path):
+      if daily_path.path.endswith(".bz2"):
+        total_size += daily_path.size
+print(total_size)
+
+# COMMAND ----------
+
+total_size // (2**30)
