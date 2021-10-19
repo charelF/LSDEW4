2b482e8de7c7382ea3921ef68fda912aaa9c503f
Charel Felte <charel.felten@gmail.com>
2021-10-19 11:07:03 +0200

    some pages

--- exploration/charel/QID_to_titles.py
+++ exploration/charel/QID_to_titles.py
@@ -1,8 +1,6 @@
 # %%
-
 import numpy as np
 import requests
-
 # %%
 def wikidata_qid_to_titles(qid):
     url = f"https://www.wikidata.org/wiki/Special:EntityData/{qid}.json"
@@ -12,13 +10,19 @@ def wikidata_qid_to_titles(qid):
         for _, sitedic in content["entities"][qid]["sitelinks"].items():
             titles.append(sitedic["title"])
     return titles
-
 # %%
 def main():
     qid = "Q131406"
+        # denial-of-service attack
+        # https://www.wikidata.org/wiki/Q131406
+    qid = "Q17329819"
+        # distributed denial-of-service attack
+        # https://www.wikidata.org/wiki/Q17329819
+        # only available in very few (5) languages, not on en.wikipedia
+
     print(wikidata_qid_to_titles(qid))
 
 if __name__ == "__main__":
     main()
-
 # %%
+
