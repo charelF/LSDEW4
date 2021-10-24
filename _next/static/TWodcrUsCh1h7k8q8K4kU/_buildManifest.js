0cb81fa016aa0333055ea71e63379834192b4e3a
gillesmag <gillesmag@users.noreply.github.com>
2021-10-24 20:32:52 +0000

    deploy: 4e7a70d5521ed0162af9a10f29fcb9868bb7f3ac

---
+++ _next/static/TWodcrUsCh1h7k8q8K4kU/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/222-32e8bb9bf6daf97e7d36.js","static/chunks/pages/index-a5ceddc6b560128cf677.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
