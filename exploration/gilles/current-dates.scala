7f79cbcc73b3d8009b7fd8dce7530a5f6bc1aeb5
Gilles Magalhaes <gilles.magalhaes@gmail.com>
2021-11-05 12:45:32 +0100

    More cleaning

--- exploration/gilles/current-dates.scala
+++
@@ -1,5 +0,0 @@
-import org.apache.spark.sql.functions._
-
-val df = spark.read.format("delta").load("/mnt/group09/pageviews.delta")
-val uniqueTimestamps = df.select(date_format(from_unixtime(col("timestamp")), "yyyy-MM-dd").alias("date")).distinct()
-uniqueTimestamps.write.mode("overwrite").parquet("/mnt/group09/current-dates.parquet")
